18ca9a3107ecc27dc31970c4cc4cc3a6bb264f71
Sean Mooney <work@seanmooney.info>
2025-05-02 15:47:01 +0100

    current config

---
+++ init.el
@@ -0,0 +1,24 @@
+;; -*- lexical-binding: t; -*-
+
+;;early init
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s seconds with %d garbage collections."
+                     (emacs-init-time "%.2f")
+                     gcs-done)))
+
+;; Silence compiler warnings as they can be pretty disruptive
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Set the right directory to store the native comp cache
+(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+
+
+;; load org package and our lit.org file
+(require 'org)
+(org-babel-load-file (expand-file-name "lit.org" user-emacs-directory))
+
